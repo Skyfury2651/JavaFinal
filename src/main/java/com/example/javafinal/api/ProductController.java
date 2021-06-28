a07f3ae4123db44db1a3bacde0b9d155b5c922b6
skyfury2651 <skyfury2651@gmail.com>
2021-06-28 21:36:55 +0700

    end

--- src/main/java/com/example/javafinal/api/ProductController.java
+++ src/main/java/com/example/javafinal/api/ProductController.java
@@ -1,8 +1,27 @@
 package com.example.javafinal.api;
 
+import com.example.javafinal.entity.Product;
+import com.example.javafinal.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
 
+    @PostMapping()
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping("/find-all")
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
 }
